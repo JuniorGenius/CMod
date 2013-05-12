81fdf1e0a2d0b337884538ad00fa2cebba9131ec
Nathan <Nathan@Nathan-PC.gateway.2wire.net>
2013-05-12 13:12:43 +1000

    Skeleton Code

--- src/main/java/me/nopresnik/plugins/cmod/CMod.java
+++ src/main/java/me/nopresnik/plugins/cmod/CMod.java
@@ -1,18 +1,13 @@
 package me.nopresnik.plugins.cmod;
 
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CMod extends JavaPlugin implements Listener {
 
+    public PlayerJoin pj = new PlayerJoin(this);
+
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
-    }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().sendMessage("Welcome, " + event.getPlayer().getDisplayName() + "!");
     }
 }
---
+++ src/main/java/me/nopresnik/plugins/cmod/PlayerJoin.java
@@ -0,0 +1,12 @@
+package me.nopresnik.plugins.cmod;
+
+import org.bukkit.event.Listener;
+
+public class PlayerJoin implements Listener {
+
+    public final CMod plugin;
+
+    PlayerJoin(CMod instance) {
+        plugin = instance;
+    }
+}
