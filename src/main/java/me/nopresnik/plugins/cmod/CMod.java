59e7ad100efa9928b6d700b398aec24c8deba818
Nathan James <Nathan@Nathan-PC.gateway.2wire.net>
2013-06-15 11:37:24 +1000

    Cleanup + Add /freeze

--- src/main/java/me/nopresnik/plugins/cmod/CMod.java
+++ src/main/java/me/nopresnik/plugins/cmod/CMod.java
@@ -12,139 +12,147 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class CMod extends JavaPlugin implements Listener
-{
+public class CMod extends JavaPlugin implements Listener {
 
-        static final Logger log = Logger.getLogger("Minecraft");
-        public List<String> mutedUsers = new ArrayList<String>();
-        public CModListener listener = new CModListener(this);
+    static final Logger log = Logger.getLogger("Minecraft");
+    public List<String> mutedUsers = new ArrayList<String>();
+    public List<String> frozenUsers = new ArrayList<String>();
+    public CModListener listener = new CModListener(this);
 
-        @Override
-        public void onEnable()
-        {
-                getServer().getPluginManager().registerEvents(listener, this);
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(listener, this);
 
-                mutedUsers.addAll(getConfig().getStringList("muted.users"));
+        mutedUsers.addAll(getConfig().getStringList("muted.users"));
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player player = (Player) sender;
+
+        if (cmd.getName().equalsIgnoreCase("colour")) {
+            Random object = new Random();
+            int color;
+            for (int counter = 1; counter <= 1; counter++) {
+                color = 1 + object.nextInt(6);
+
+                if (color == 1) {
+                    player.setDisplayName(ChatColor.RED + player.getName());
+                    player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.RED + "red" + ChatColor.GRAY + "!");
+                }
+                if (color == 2) {
+                    player.setDisplayName(ChatColor.BLUE + player.getName());
+                    player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.BLUE + "blue" + ChatColor.GRAY + "!");
+                }
+                if (color == 3) {
+                    player.setDisplayName(ChatColor.DARK_GREEN + player.getName());
+                    player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.DARK_GREEN + "green" + ChatColor.GRAY + "!");
+                }
+                if (color == 4) {
+                    player.setDisplayName(ChatColor.YELLOW + player.getName());
+                    player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.YELLOW + "yellow" + ChatColor.GRAY + "!");
+                }
+                if (color == 5) {
+                    player.setDisplayName(ChatColor.AQUA + player.getName());
+                    player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.AQUA + "aqua" + ChatColor.GRAY + "!");
+                }
+                if (color == 6) {
+                    player.setDisplayName(ChatColor.GRAY + player.getName());
+                    player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now gray!");
+                }
+            }
+            return true;
         }
 
-        @Override
-        public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-        {
-                Player player = (Player) sender;
+        if (sender.hasPermission("CMod.mod") || sender.isOp()) {
+            Player target = getServer().getPlayer(args[0]);
+            String targetName = target.getName().toLowerCase();
+            if (cmd.getName().equalsIgnoreCase("freeze")) {
+                if (args.length == 1) {
+                    if (target != null) {
+                        freezePlayer(targetName);
+                        target.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You have been frozen by " + ChatColor.YELLOW + sender.getName());
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You must include a player's name");
+                    return true;
+                }
+            }
+            if (cmd.getName().equalsIgnoreCase("unfreeze")) {
+                if (args.length == 1) {
+                    if (target != null) {
+                        unFreezePlayer(targetName);
+                        target.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You have been unfrozen by " + ChatColor.YELLOW + sender.getName());
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You must include a player's name");
+                    return true;
+                }
+            }
+            if (cmd.getName().equalsIgnoreCase("mute")) {
+                if (args.length == 1) {
 
-                if (cmd.getName().equalsIgnoreCase("colour"))
-                {
-                        Random object = new Random();
-                        int color;
-                        for (int counter = 1; counter <= 1; counter++)
-                        {
-                                color = 1 + object.nextInt(6);
-
-                                if (color == 1)
-                                {
-                                        player.setDisplayName(ChatColor.RED + player.getName());
-                                        player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.RED + "red" + ChatColor.GRAY + "!");
-                                }
-                                if (color == 2)
-                                {
-                                        player.setDisplayName(ChatColor.BLUE + player.getName());
-                                        player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.BLUE + "blue" + ChatColor.GRAY + "!");
-                                }
-                                if (color == 3)
-                                {
-                                        player.setDisplayName(ChatColor.DARK_GREEN + player.getName());
-                                        player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.DARK_GREEN + "green" + ChatColor.GRAY + "!");
-                                }
-                                if (color == 4)
-                                {
-                                        player.setDisplayName(ChatColor.YELLOW + player.getName());
-                                        player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.YELLOW + "yellow" + ChatColor.GRAY + "!");
-                                }
-                                if (color == 5)
-                                {
-                                        player.setDisplayName(ChatColor.AQUA + player.getName());
-                                        player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.AQUA + "aqua" + ChatColor.GRAY + "!");
-                                }
-                                if (color == 6)
-                                {
-                                        player.setDisplayName(ChatColor.GRAY + player.getName());
-                                        player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now gray!");
-                                }
+                    if (target != null) {
+                        if (!mutedUsers.contains(targetName) && target.isOnline()) {
+                            mutePlayer(targetName);
+                            sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You have muted " + ChatColor.BLUE + target.getName());
+                            target.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You have been muted by " + ChatColor.BLUE + sender.getName());
+                            return true;
                         }
+                    }
+                }
+                sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You must include a player. " + ChatColor.DARK_GREEN + "/mute <player>");
+                return true;
+            }
+
+            if (cmd.getName().equalsIgnoreCase("unmute")) {
+                if (args.length == 1) {
+                    if (target != null) {
+                        if (mutedUsers.contains(args[0])) {
+                            removeMuteUser(targetName);
+                            sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You have unmuted " + ChatColor.BLUE + target.getName());
+                            target.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You have been unmuted by " + ChatColor.BLUE + sender.getName());
+                            return true;
+                        } else {
+                            sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "The player \"" + ChatColor.YELLOW + args[0].toString() + ChatColor.GRAY + "\" is not muted");
+                            sender.sendMessage(ChatColor.GRAY + "     Make sure you're typing the full name");
+                            return true;
+                        }
+                    } else {
+                        sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "The player \"" + ChatColor.YELLOW + args[0].toString() + ChatColor.GRAY + "\" is not online.");
                         return true;
+                    }
                 }
+                sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You must include a player. " + ChatColor.DARK_GREEN + "/unmute <player>");
+                return true;
+            }
 
-                if (sender.hasPermission("CMod.mod") || sender.isOp())
-                {
-                        if (cmd.getName().equalsIgnoreCase("mute"))
-                        {
-                                if (args.length == 1)
-                                {
-                                        Player target = getServer().getPlayer(args[0]);
-                                        if (target != null)
-                                        {
-                                                String targetName = target.getName().toLowerCase();
-                                                if (!mutedUsers.contains(targetName) && target.isOnline())
-                                                {
-                                                        mutePlayer(targetName);
-                                                        sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You have muted " + ChatColor.BLUE + target.getName());
-                                                        target.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You have been muted by " + ChatColor.BLUE + sender.getName());
-                                                        return true;
-                                                }
-                                        }
-                                }
-                                sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You must include a player. " + ChatColor.DARK_GREEN + "/mute <player>");
-                                return true;
-                        }
-
-                        if (cmd.getName().equalsIgnoreCase("unmute"))
-                        {
-                                if (args.length == 1)
-                                {
-                                        Player target = getServer().getPlayer(args[0]);
-                                        if (target != null)
-                                        {
-                                                if (mutedUsers.contains(args[0]))
-                                                {
-                                                        String targetName = target.getName().toLowerCase();
-                                                        removeMuteUser(targetName);
-                                                        sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You have unmuted " + ChatColor.BLUE + target.getName());
-                                                        target.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You have been unmuted by " + ChatColor.BLUE + sender.getName());
-                                                        return true;
-                                                } else
-                                                {
-                                                        sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "The player \"" + ChatColor.YELLOW + args[0].toString() + ChatColor.GRAY + "\" is not muted");
-                                                        sender.sendMessage(ChatColor.GRAY + "     Make sure you're typing the full name");
-                                                        return true;
-                                                }
-                                        } else
-                                        {
-                                                sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "The player \"" + ChatColor.YELLOW + args[0].toString() + ChatColor.GRAY + "\" is not online.");
-                                                return true;
-                                        }
-                                }
-                                sender.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You must include a player. " + ChatColor.DARK_GREEN + "/unmute <player>");
-                                return true;
-                        }
-
-                }
-                return false;
+        } else {
+            sender.sendMessage(ChatColor.DARK_RED + "You do not have permission.");
+            return true;
         }
+        return false;
+    }
 
-        private void mutePlayer(String player)
-        {
-                mutedUsers.add(player.toLowerCase());
-                getConfig().set("muted.users", mutedUsers.toArray());
-                saveConfig();
-        }
+    private void mutePlayer(String player) {
+        mutedUsers.add(player.toLowerCase());
+        getConfig().set("muted.users", mutedUsers.toArray());
+        saveConfig();
+    }
 
-        private void removeMuteUser(String player)
-        {
-                if (mutedUsers.contains(player.toLowerCase()))
-                {
-                        mutedUsers.remove(player.toLowerCase());
-                        getConfig().set("muted.users", mutedUsers.toArray());
-                }
-                saveConfig();
+    private void freezePlayer(String player) {
+        frozenUsers.add(player.toLowerCase());
+    }
+
+    private void unFreezePlayer(String player) {
+        frozenUsers.remove(player.toLowerCase());
+    }
+
+    private void removeMuteUser(String player) {
+        if (mutedUsers.contains(player.toLowerCase())) {
+            mutedUsers.remove(player.toLowerCase());
+            getConfig().set("muted.users", mutedUsers.toArray());
         }
+        saveConfig();
+    }
 }
--- src/main/java/me/nopresnik/plugins/cmod/CModListener.java
+++ src/main/java/me/nopresnik/plugins/cmod/CModListener.java
@@ -11,139 +11,127 @@ import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-public class CModListener implements Listener
-{
+public class CModListener implements Listener {
 
-        public final CMod plugin;
+    public final CMod plugin;
 
-        CModListener(CMod instance)
-        {
-                plugin = instance;
+    CModListener(CMod instance) {
+        plugin = instance;
+    }
+
+    @EventHandler
+    public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
+
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+
+        if (plugin.mutedUsers.contains(player.getName().toLowerCase())) {
+            event.setCancelled(true);
+            player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You're not permitted to talk while muted.");
+            plugin.log.info("Blocked Message: " + "<" + event.getPlayer().getName() + "> " + event.getMessage());
         }
 
-        @EventHandler
-        public void onAsyncPlayerChat(AsyncPlayerChatEvent event)
-        {
-
-                Player player = event.getPlayer();
-                String message = event.getMessage();
-
-                if (plugin.mutedUsers.contains(player.getName().toLowerCase()))
-                {
-                        event.setCancelled(true);
-                        player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "You're not permitted to talk while muted.");
-                        plugin.log.info("Blocked Message: " + "<" + event.getPlayer().getName() + "> " + event.getMessage());
-                }
-
-                int upperCount = 0;
-                for (int i = 0; i < message.length(); i++)
-                {
-                        if (Character.isUpperCase(message.charAt(i)))
-                        {
-                                upperCount++;
-                        }
-                }
-
-                if ((upperCount > message.length() / 2) && message.length() > 8)
-                {
-                        player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Please do not type in all caps.");
-                        event.setCancelled(true);
-                }
-
-                event.setFormat("<" + player.getDisplayName() + ChatColor.WHITE + "> " + message);
+        int upperCount = 0;
+        for (int i = 0; i < message.length(); i++) {
+            if (Character.isUpperCase(message.charAt(i))) {
+                upperCount++;
+            }
         }
 
-        @EventHandler
-        public void onItemSpawn(ItemSpawnEvent event)
-        {
-                event.setCancelled(true);
+        if ((upperCount > message.length() / 2) && message.length() > 8) {
+            player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Please do not type in all caps.");
+            event.setCancelled(true);
         }
 
-        @EventHandler
-        public void onPlayerJoin(PlayerJoinEvent event)
-        {
-                Player player = event.getPlayer();
-                event.setJoinMessage(ChatColor.RED + "[PlayerJoin] " + ChatColor.GRAY + player.getName());
+        event.setFormat("<" + player.getDisplayName() + ChatColor.WHITE + "> " + message);
+    }
 
-                Random object = new Random();
-                int color;
-                for (int counter = 1; counter <= 1; counter++)
-                {
-                        color = 1 + object.nextInt(6);
+    @EventHandler
+    public void onItemSpawn(ItemSpawnEvent event) {
+        event.setCancelled(true);
+    }
 
-                        if (color == 1)
-                        {
-                                player.setDisplayName(ChatColor.RED + player.getName());
-                                player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.RED + "red" + ChatColor.GRAY + "!");
-                        }
-                        if (color == 2)
-                        {
-                                player.setDisplayName(ChatColor.BLUE + player.getName());
-                                player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.BLUE + "blue" + ChatColor.GRAY + "!");
-                        }
-                        if (color == 3)
-                        {
-                                player.setDisplayName(ChatColor.DARK_GREEN + player.getName());
-                                player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.DARK_GREEN + "green" + ChatColor.GRAY + "!");
-                        }
-                        if (color == 4)
-                        {
-                                player.setDisplayName(ChatColor.YELLOW + player.getName());
-                                player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.YELLOW + "yellow" + ChatColor.GRAY + "!");
-                        }
-                        if (color == 5)
-                        {
-                                player.setDisplayName(ChatColor.AQUA + player.getName());
-                                player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.AQUA + "aqua" + ChatColor.GRAY + "!");
-                        }
-                        if (color == 6)
-                        {
-                                player.setDisplayName(ChatColor.GRAY + player.getName());
-                                player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now gray!");
-                        }
-                }
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        event.setJoinMessage(ChatColor.RED + "[PlayerJoin] " + ChatColor.GRAY + player.getName());
+
+        Random object = new Random();
+        int color;
+        for (int counter = 1; counter <= 1; counter++) {
+            color = 1 + object.nextInt(6);
+
+            if (color == 1) {
+                player.setDisplayName(ChatColor.RED + player.getName());
+                player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.RED + "red" + ChatColor.GRAY + "!");
+            }
+            if (color == 2) {
+                player.setDisplayName(ChatColor.BLUE + player.getName());
+                player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.BLUE + "blue" + ChatColor.GRAY + "!");
+            }
+            if (color == 3) {
+                player.setDisplayName(ChatColor.DARK_GREEN + player.getName());
+                player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.DARK_GREEN + "green" + ChatColor.GRAY + "!");
+            }
+            if (color == 4) {
+                player.setDisplayName(ChatColor.YELLOW + player.getName());
+                player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.YELLOW + "yellow" + ChatColor.GRAY + "!");
+            }
+            if (color == 5) {
+                player.setDisplayName(ChatColor.AQUA + player.getName());
+                player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now " + ChatColor.AQUA + "aqua" + ChatColor.GRAY + "!");
+            }
+            if (color == 6) {
+                player.setDisplayName(ChatColor.GRAY + player.getName());
+                player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Your chat name is now gray!");
+            }
         }
+    }
 
-        @EventHandler
-        public void onPlayerQuit(PlayerQuitEvent event)
-        {
-                Player player = event.getPlayer();
-                event.setQuitMessage(ChatColor.RED + "[PlayerQuit] " + ChatColor.GRAY + player.getName());
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        event.setQuitMessage(ChatColor.RED + "[PlayerQuit] " + ChatColor.GRAY + player.getName());
+    }
+
+    @EventHandler
+    public void onPlayerKick(PlayerKickEvent event) {
+        Player player = event.getPlayer();
+        event.setLeaveMessage(ChatColor.RED + "[PlayerQuit] " + ChatColor.GRAY + player.getName());
+    }
+
+    @EventHandler
+    public void onWitherSpawn(CreatureSpawnEvent event) {
+        if ((event.getEntityType() == EntityType.WITHER) && (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.BUILD_WITHER)) {
+            event.setCancelled(true);
         }
+    }
 
-        @EventHandler
-        public void onPlayerKick(PlayerKickEvent event)
-        {
-                Player player = event.getPlayer();
-                event.setLeaveMessage(ChatColor.RED + "[PlayerQuit] " + ChatColor.GRAY + player.getName());
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (plugin.frozenUsers.contains(player.getName().toLowerCase())) {
+            event.setCancelled(true);
         }
-
-        @EventHandler
-        public void onWitherSpawn(CreatureSpawnEvent event)
-        {
-                if ((event.getEntityType() == EntityType.WITHER) && (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.BUILD_WITHER))
-                {
-                        event.setCancelled(true);
-                }
-        }
-
-        /*@EventHandler
-         public void playerInteract(PlayerInteractEvent event) {
-         if (event.getPlayer().hasPermission("CMod.trusted")) {
-         Block b = event.getClickedBlock();
-         if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-         b.setType(Material.AIR);
-         b.getRelative(BlockFace.NORTH).setType(Material.AIR);
-         b.getRelative(BlockFace.NORTH_EAST).setType(Material.AIR);
-         b.getRelative(BlockFace.NORTH_WEST).setType(Material.AIR);
-         b.getRelative(BlockFace.SOUTH).setType(Material.AIR);
-         b.getRelative(BlockFace.SOUTH_EAST).setType(Material.AIR);
-         b.getRelative(BlockFace.SOUTH_WEST).setType(Material.AIR);
-         b.getRelative(BlockFace.EAST).setType(Material.AIR);
-         b.getRelative(BlockFace.WEST).setType(Material.AIR);;
-         }
-         }
-         }*/
+    }
+    /*@EventHandler
+     public void playerInteract(PlayerInteractEvent event) {
+     if (event.getPlayer().hasPermission("CMod.trusted")) {
+     Block b = event.getClickedBlock();
+     if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+     b.setType(Material.AIR);
+     b.getRelative(BlockFace.NORTH).setType(Material.AIR);
+     b.getRelative(BlockFace.NORTH_EAST).setType(Material.AIR);
+     b.getRelative(BlockFace.NORTH_WEST).setType(Material.AIR);
+     b.getRelative(BlockFace.SOUTH).setType(Material.AIR);
+     b.getRelative(BlockFace.SOUTH_EAST).setType(Material.AIR);
+     b.getRelative(BlockFace.SOUTH_WEST).setType(Material.AIR);
+     b.getRelative(BlockFace.EAST).setType(Material.AIR);
+     b.getRelative(BlockFace.WEST).setType(Material.AIR);;
+     }
+     }
+     }*/
 }
