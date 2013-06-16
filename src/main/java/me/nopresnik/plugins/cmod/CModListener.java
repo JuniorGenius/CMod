ee4037ab341e20b7458e8048514f03e57703c1fc
Nathan James <Nathan@Nathan-PC.gateway.2wire.net>
2013-06-16 18:54:26 +1000

    Might fix our % issue.

--- src/main/java/me/nopresnik/plugins/cmod/CModListener.java
+++ src/main/java/me/nopresnik/plugins/cmod/CModListener.java
@@ -5,6 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.ItemSpawnEvent;
@@ -22,7 +23,7 @@ public class CModListener implements Listener {
         plugin = instance;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
 
         Player player = event.getPlayer();
