fb0d8104988ba29f5c7bc44b7466067c5041e086
JuniorGenius <drgenius@ca.rr.com>
2013-06-19 02:43:24 -0600

    Lower Caps
    Made it so that a message in all caps will be lowered, capitalized, and sent with a warning, therefore not making the sender type it over again.

--- src/main/java/me/nopresnik/plugins/cmod/CModListener.java
+++ src/main/java/me/nopresnik/plugins/cmod/CModListener.java
@@ -44,8 +44,11 @@ public class CModListener implements Listener {
         }
 
         if ((upperCount > message.length() / 2) && message.length() > 8) {
+            message = message.toLowerCase()
+            message = message.substring(0,1).toUpperCase()
+            event.setCancelled(false);
             player.sendMessage(ChatColor.RED + "[C] " + ChatColor.GRAY + "Please do not type in all caps.");
-            event.setCancelled(true);
+            //event.setCancelled(true);
         }
 
         event.setFormat("<" + player.getDisplayName() + ChatColor.WHITE + "> " + message);
